24b7f448f2fa0a6eb7446d450a3445a375739395
jikefan <1791504346@qq.com>
2025-01-15 18:11:18 +0800

    study context

--- context/withcancel_test.go
+++ context/withcancel_test.go
@@ -1,1 +1,29 @@
 package context
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func Speak(ctx context.Context) {
+	for range time.Tick(time.Second) {
+		select {
+		case <-ctx.Done():
+			fmt.Println("我闭嘴了")
+			return
+		default:
+			fmt.Println("balabalabalabala")
+		}
+	}
+}
+
+func TestWithCancel(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	go Speak(ctx)
+
+	time.Sleep(3 * time.Second)
+	cancel()
+	time.Sleep(2 * time.Second)
+}
